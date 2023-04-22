231e7b449318a236c88a42bf527709f1af9d876b
ZPaul2Fresh8 <zpaul2fresh8@gmail.com>
2023-04-22 16:55:45 -0400

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use bevy::prelude::*;
+
 fn main() {
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_system(hello_world)
+    .run()
+}
+
+pub fn hello_world(){
     println!("Hello, world!");
 }
