625c53fa4a8bfd93373a3e6a7fc0aec588ea690e
ZPaul2Fresh8 <zpaul2fresh8@gmail.com>
2023-04-22 17:31:39 -0400

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,45 @@
 use bevy::prelude::*;
+use bevy::render::view::window;
+use bevy::transform::commands;
+use bevy::window::PrimaryWindow;
 
 fn main() {
     App::new()
     .add_plugins(DefaultPlugins)
-    .add_system(hello_world)
-    .run()
+    .add_startup_system(spawn_sprite)
+    .add_startup_system(spawn_camera)
+    .run();
 }
 
-pub fn hello_world(){
-    println!("Hello, world!");
+#[derive(Component)]
+pub struct Fighter{}
+
+pub fn spawn_sprite(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(
+        (
+            SpriteBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            texture: asset_server.load("gfx/II.png"),
+            ..default()
+            },
+            Fighter {},
+        )
+    );
 }
+
+pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>)
+{
+    let window = window_query.get_single().unwrap();
+    commands.spawn(
+        Camera2dBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            ..default()
+        }
+    );
+}    
